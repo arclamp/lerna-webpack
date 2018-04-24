1bf486e003e7b09c4adfb7382df2bc1afe508510
Roni Choudhury <roni.choudhury@kitware.com>
2018-04-24 11:53:58 -0400

    Use html-webpack-plugin for test app

--- packages/padgraph/webpack.config.js
+++ packages/padgraph/webpack.config.js
@@ -1,9 +1,16 @@
 var path = require('path');
+var HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve('dist'),
     filename: 'padgraph.js'
-  }
+  },
+  plugins: [
+    new HtmlPlugin({
+      title: 'PadGraph',
+      filename: 'index.html'
+    })
+  ]
 };
